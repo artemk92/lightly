03574374d107ade79d1c39e9197a3f7f7c0a02ce
artemk92 <artemk9234@gmail.com>
2024-07-08 20:58:53 +0500

    initial commit

---
+++ src/main/java/com/temcore/Lightly/LightlyApplication.java
@@ -0,0 +1,13 @@
+package com.temcore.Lightly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LightlyApplication.class, args);
+	}
+
+}
